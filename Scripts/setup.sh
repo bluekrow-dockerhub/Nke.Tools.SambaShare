8a2613f2a4a0cc0c74ff6d8a75691dccfb39069f
BlueKrow-DockerHub <ndioses+dockerhub@gmail.com>
2021-04-12 00:32:06 +0200

    refactor: allow repeated execution on local run

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -3,7 +3,6 @@
 echo "Create sharing directory"
 mkdir -p /media/storage
 chmod 0777 /media/storage
-echo "shared content" > /media/storage/shared-file.txt
 
 echo "Create local group and user"
 addgroup -g 1000 smbgroup
--- run-local.sh
+++ run-local.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-echo "SAMBA SHARE CONTAINER"
+if [ "$#" -eq 0 ]; then
+  echo "Error: must supply a Volume Name or Path."
+  exit 1
+fi
 
-echo "Running container"
+if [ "$#" -ne 1 ]; then
+  echo "Error: Too many arguments. Only one argument expected."
+  exit 1
+fi
+
+SHARED_VOL=$1
+CONTAINER_NAME="sambashare"
+IMAGE_NAME="nke-tools-sambashare"
+
+echo "SAMBA SHARE CONTAINER"
+if [ "$(sudo docker ps -q -f name=$CONTAINER_NAME)" ]; then
+  echo "Stopping currently running container"
+  sudo docker stop $CONTAINER_NAME
+fi
+
+if [ "$(sudo docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
+  echo "Removing currently running container"
+  sudo docker rm $CONTAINER_NAME
+fi
+
+echo "Starting container. Sharing $SHARED_VOL"
 sudo docker run \
   -dt \
   -p 445:445 \
-  --name sambashare \
+  --name $CONTAINER_NAME \
   --rm \
-  nke-tools-sambashare
-
+  --volume $SHARED_VOL:/media/storage \
+  $IMAGE_NAME
 echo "Container is running"
